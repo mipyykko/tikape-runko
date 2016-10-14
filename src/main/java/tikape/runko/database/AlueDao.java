c11e6d8138a8f5b87b9e7b9ba2fd26e0872f6d18
Pyykkö Mikko P <mikko.pyykko@helsinki.fi>
2016-10-14 13:46:24 +0300

    templatet lisätty

--- src/main/java/tikape/runko/database/AlueDao.java
+++ src/main/java/tikape/runko/database/AlueDao.java
@@ -5,10 +5,29 @@
  */
 package tikape.runko.database;
 
+import java.sql.SQLException;
+import java.util.List;
+import tikape.runko.domain.Alue;
+
 /**
  *
  * @author pyykkomi
  */
-public class AlueDao {
+public class AlueDao implements Dao<Alue, Integer> {
+
+    @Override
+    public Alue findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Alue> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/main/java/tikape/runko/database/ViestiDao.java
+++ src/main/java/tikape/runko/database/ViestiDao.java
@@ -5,10 +5,29 @@
  */
 package tikape.runko.database;
 
+import java.sql.SQLException;
+import java.util.List;
+import tikape.runko.domain.Viesti;
+
 /**
  *
  * @author pyykkomi
  */
-public class ViestiDao {
+public class ViestiDao implements Dao<Viesti, Integer>{
+
+    @Override
+    public Viesti findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Viesti> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
