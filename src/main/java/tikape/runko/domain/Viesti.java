44073567028b5d21c89e496c42010e47a18e8390
Pyykkö Mikko P <mikko.pyykko@helsinki.fi>
2016-10-21 15:17:39 +0300

    oikeat polut main.javaan

--- src/main/java/tikape/runko/domain/Viesti.java
+++ src/main/java/tikape/runko/domain/Viesti.java
@@ -1,9 +1,29 @@
 package tikape.runko.domain;
 
-/**
- *
- * @author pyykkomi
- */
 public class Viesti {
+    
+    private Integer id;
+    private String nimi;
+
+    public Viesti(Integer id, String nimi) {
+        this.id = id;
+        this.nimi = nimi;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
     
 }
